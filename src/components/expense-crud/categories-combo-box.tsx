66a80529ee017b446b2779de434b74e199aeada5
John P <johnrprutton@gmail.com>
2023-10-18 12:34:19 +0200

    feat: add category combobox

---
+++ src/components/expense-crud/categories-combo-box.tsx
@@ -0,0 +1,79 @@
+"use client"
+
+import { useState } from "react"
+import { Check, ChevronsUpDown } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import type { Category } from "@/lib/db/schema/categories"
+
+export function CategoryComboBox({ categories }: { categories: Category[] }) {
+  const [open, setOpen] = useState(false)
+  const [categoryId, setCategoryId] = useState<Category["id"] | null>(null)
+
+  function updateSelected(newCategoryDescription: Category["description"]) {
+    const newId = categories.find(
+      (category) =>
+        category.description.toLowerCase() === newCategoryDescription
+    )?.id
+
+    setCategoryId(!newId || newId === categoryId ? null : newId)
+    setOpen(false)
+  }
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-full justify-between"
+        >
+          {categoryId
+            ? categories.find((category) => category.id === categoryId)
+                ?.description
+            : "Select a category..."}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+
+      <PopoverContent className="w-full p-0">
+        <Command>
+          <CommandInput placeholder="Search for a category..." />
+
+          <CommandEmpty>No categories found.</CommandEmpty>
+
+          <CommandGroup>
+            {categories.map((category) => (
+              <CommandItem
+                key={category.id + category.description}
+                onSelect={updateSelected}
+              >
+                <Check
+                  className={cn(
+                    "mr-2 h-4 w-4",
+                    category.id === categoryId ? "opacity-100" : "opacity-0"
+                  )}
+                />
+                {category.description}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
+}
