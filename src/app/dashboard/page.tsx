837afd2163d13f6d477c2e6f8beb59058a36597c
John P <johnrprutton@gmail.com>
2023-10-22 11:51:11 +0200

    refactor: remove initialExpense from expenseList

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,15 @@
-import { getExpensesByUser } from "@/lib/api/expenses/queries"
-
 import { Chart } from "@/components/chart"
 import { ExpenseCrud } from "@/components/expense-crud"
 import { ExpenseList } from "@/components/expense-list"
 
-export default async function DashboardPage() {
-  const initialExpenses = await getExpensesByUser()
+export default function DashboardPage() {
   return (
     <main>
       <Chart />
 
       <ExpenseCrud />
 
-      <ExpenseList initialExpenses={initialExpenses} />
+      <ExpenseList />
     </main>
   )
 }
