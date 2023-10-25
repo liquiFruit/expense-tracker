96330d5ed391e0e0f570df3e09718af81492c4e9
John P <johnrprutton@gmail.com>
2023-10-26 00:38:00 +0200

    test: add ownership test for deleteExpense

--- src/lib/api/expenses/mutations.test.ts
+++ src/lib/api/expenses/mutations.test.ts
@@ -1,21 +1,46 @@
-import { deleteExpense } from "./mutations"
-
-jest.mock("../../db", () => ({
-  db: {
-    select: jest.fn().mockImplementation(() => ({
-      from: (_: any) => ["Personal", "Other"],
-    })),
-  },
-}))
-
-jest.mock("../../auth/utils", () => ({
-  getUserAuth: jest.fn().mockImplementation(async () => ({
-    session: null,
-  })),
-}))
+beforeEach(() => jest.resetModules())
 
 describe("deleteExpense", () => {
   it("should fail if user is unauthed", async () => {
+    jest.mock("../../db", () => ({
+      db: {},
+    }))
+    jest.mock("../../auth/utils", () => ({
+      getUserAuth: jest.fn().mockImplementation(async () => ({
+        session: null,
+      })),
+    }))
+
+    const { deleteExpense } = require("./mutations")
     expect(await deleteExpense(0)).toEqual({ error: "Unauthorized" })
+  })
+
+  it("should fail if user does not own the provided expense", async () => {
+    jest.mock("../../auth/utils", () => ({
+      getUserAuth: jest.fn().mockImplementation(async () => ({
+        session: {
+          user: {
+            id: "mocked_user_id",
+          },
+        },
+      })),
+    }))
+
+    jest.mock("../../db", () => ({
+      db: {
+        query: {
+          expenses: {
+            findFirst: jest.fn(() => undefined),
+          },
+        },
+      },
+    }))
+
+    const { deleteExpense } = require("./mutations")
+    const { db } = require("../../db")
+
+    expect(await deleteExpense(1)).toEqual({
+      error: "You are not authorised to mutate this expense",
+    })
   })
 })
